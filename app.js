28720f9fc4073a6543fb8cedb59c8fa61fbdab5c
Keith <kbarber34@ivytech.edu>
2024-03-02 11:01:48 -0500

    partial sign up functionality

--- app.js
+++ app.js
@@ -23,21 +23,3 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-//cookies
-app.get('/set-cookies', (req, res) => {
-    res.cookie("newUser", false);
-    res.cookie("isEmployee", true, {
-        maxAge: 1000*60*60*24,
-        httpOnly: true
-    });
-
-    res.send("You got the cookies");
-});
-
-app.get('/read-cookies', (req, res) => {
-    const cookies = req.cookies;
-    console.log(cookies);
-    
-    res.json(cookies);
-});
