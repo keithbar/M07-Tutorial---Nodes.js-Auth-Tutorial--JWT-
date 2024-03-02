ae956d427632a139e43403ba64ae46d4206eb82e
Keith <kbarber34@ivytech.edu>
2024-03-01 20:19:10 -0500

    create user schema

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://kbarber34:GqfsU5ySDoU3onwL@sdev255.gxv9ds4.mongodb.net/m07tutorial?retryWrites=true&w=majority&appName=SDEV255';
+const dbURI = 'mongodb+srv://kbarber34:GqfsU5ySDoU3onwL@sdev255.gxv9ds4.mongodb.net/m07tutorial';
 mongoose.connect(dbURI)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
@@ -18,3 +20,4 @@ mongoose.connect(dbURI)
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
