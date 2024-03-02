23aa07ee18db2703af263e8883a2d87f271b4031
Keith <kbarber34@ivytech.edu>
2024-03-02 10:47:24 -0500

    cookie testing

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -14,10 +16,28 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb+srv://kbarber34:GqfsU5ySDoU3onwL@sdev255.gxv9ds4.mongodb.net/m07tutorial';
 mongoose.connect(dbURI)
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//cookies
+app.get('/set-cookies', (req, res) => {
+    res.cookie("newUser", false);
+    res.cookie("isEmployee", true, {
+        maxAge: 1000*60*60*24,
+        httpOnly: true
+    });
+
+    res.send("You got the cookies");
+});
+
+app.get('/read-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+    
+    res.json(cookies);
+});
